65dfbedcfdf293cdcc1f2063554f613db951465d
Steve Prentice <stevejprentice@gmail.com>
2013-12-20 17:27:14 +0000

    json serialization of DTOs

--- source/Publisher.Console/Program.cs
+++ source/Publisher.Console/Program.cs
@@ -1,25 +1,49 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Framing.Impl.v0_9;
+using ReferenceData.API;
+using Thrift.Protocol;
 
 namespace Publisher.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //var factory = new ConnectionFactory() { HostName = "localhost" };
-            var factory = new ConnectionFactory() { Address = "192.168.1.111" }; using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            var factory = new ConnectionFactory { Address = "192.168.1.111" };
+            using (var connection = factory.CreateConnection())
             {
-                channel.ExchangeDeclare("logs", "fanout");
-
-                while (true)
+                using (var channel = connection.CreateModel())
                 {
-                    var message = System.Console.ReadLine();
-                    var body = Encoding.UTF8.GetBytes(message);
-                    var basicProperties = channel.CreateBasicProperties();
-                    basicProperties.SetPersistent(true);
-                    channel.BasicPublish("logs", "", basicProperties, body);
+                    channel.ExchangeDeclare("logs", "fanout");
+
+                    while (true)
+                    {
+                        var message = System.Console.ReadLine();
+
+                        var agency = new Agency
+                        {
+                            Name = message
+                        };
+
+                        var person = new Person
+                        {
+                            Name = message
+                        };
+
+                        var json = Serializer.Serialize(new List<TBase> { agency, person });
+
+                        var body = Encoding.UTF8.GetBytes(json);
+                        var basicProperties = channel.CreateBasicProperties();
+                        basicProperties.SetPersistent(true);
+                        channel.BasicPublish("logs", "", basicProperties, body);
+                    }
                 }
             }
         }
